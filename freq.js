7f25bae5e6b5d7067d197db2067e12b9b7f1139c
Hiroshi SUDA <suda@net.it-chiba.ac.jp>
2016-06-16 23:03:59 +0900

    mediaDevicesの有無によって処理を分けてみた

--- freq.js
+++ freq.js
@@ -1,26 +1,42 @@
-//　AudioContext
+// AudioContext
 var audioCtx = new AudioContext();
 
-//　MediaStreamSource
+// MediaStreamSource
 
-//　AnalyserNodeを生成
+// AnalyserNodeを生成
 var analyser = audioCtx.createAnalyser();
 
-//　マイクの音を取り込む処理
+// マイクの音を取り込む処理
 function mic() {
-    navigator.GetUserMedia({
+	//var getUserMedia = navigator.mediaDevices.getUserMedia || navigator.getUserMeida || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+	//navigator.getUserMedia = navigator.getUserMeida || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+	
+	if( navigator.mediaDevices.getUserMedia ) {
+		navigator.mediaDevices.getUserMedia( {
+			audio: true
+		}).then( function(stream) {
+            // MediaStreamSourceNodeを生成し，マイクからの入力を格納する
+            var mediastreamsource = audioCtx.createMediaStreamSource(stream);
+            // MediaStreamSourceNode→AnalyserNode
+            mediastreamsource.connect(analyser);
+		}).catch( function(reason) {
+            console.log("error");
+		});
+	} else {
+		navigator.getUserMedia = navigator.getUserMeida || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    navigator.getUserMedia({
             audio: true
         },
         function(stream) {
-            //　MediaStreamSourceNodeを生成し，マイクからの入力を格納する
+            // MediaStreamSourceNodeを生成し，マイクからの入力を格納する
             var mediastreamsource = audioCtx.createMediaStreamSource(stream);
-            //　MediaStreamSourceNode→AnalyserNode
+            // MediaStreamSourceNode→AnalyserNode
             mediastreamsource.connect(analyser);
         },
         function(e) {
             console.log("error");
         }
-    );
+    );}
 }
 
 window.addEventListener("load", mic, false);
