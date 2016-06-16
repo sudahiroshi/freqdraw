c49af3f79d90af5ce4096b45cede132e57962077
Hiroshi SUDA <suda@net.it-chiba.ac.jp>
2016-06-16 23:30:22 +0900

    ゴミを削除

--- freq.js
+++ freq.js
@@ -8,9 +8,6 @@ var analyser = audioCtx.createAnalyser();
 
 // マイクの音を取り込む処理
 function mic() {
-	//var getUserMedia = navigator.mediaDevices.getUserMedia || navigator.getUserMeida || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-	//navigator.getUserMedia = navigator.getUserMeida || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-	
 	if( navigator.mediaDevices.getUserMedia ) {
 		navigator.mediaDevices.getUserMedia( {
 			audio: true
